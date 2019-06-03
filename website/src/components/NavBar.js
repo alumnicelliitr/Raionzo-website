614f311a010259cba63356e7dda5f8c3c34e508f
Kunal Mohan <kunalmohan99@gmail.com>
2019-06-04 01:02:03 +0530

    Navigation bar added

---
+++ website/src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import raionzoLogo from './images/raionzologo.png';
+import './css/NavBar.css';
+
+class NavBar extends Component {
+	render(){
+		return(
+			<div className="nav-bar">
+			<img className="raionzo-logo" src={raionzoLogo} alt="Raionzo Logo" />
+			<div className="head">Raionzo</div>
+			<div className="nav-links">
+				<NavLink name="Home" />
+				<NavLink name="Events" />
+				<NavLink name="Schedule" />
+				<NavLink name="Contact Us" />
+			</div>
+			<div className="reg-link">Register</div>
+			</div>
+		);
+	}
+}
+
+function NavLink(props){
+	return(
+		<div className="nav-link">
+			{props.name}
+		</div>
+	);
+}
+
+export default NavBar;
