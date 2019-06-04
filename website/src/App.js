87f5f75527e8b478c58e3147a17b8d4724dd040a
abhayraghuwanshi <araghuwanshi@ee.iitr.ac.in>
2019-06-04 21:49:50 +0530

    added body for main page

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './components/NavBar.js';
 import './App.css';
+import MainContainer from './components/maincontainer.js'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <MainContainer/>
     </div>
   );
 }
---
+++ website/src/components/container_3.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './css/container_3.css';
+import group from './images/Group.png'
+
+
+class CONTAINER3 extends React.Component {
+    render() {
+      return (
+        <div  className="container3">
+            <div className="container-3-child-heading"> About Raionzo</div>
+                <div className="container-3-parent">
+                    
+                    <div className="container-3-child-1" ><img className="container-3-child" src={group}></img></div> 
+                    
+                    <div className="container-3-child-2">
+                        <div className="container-3-child-body"> <p>RAIONZO, the student-alumni fest being organized for the first time this year by the Institute Alumni Relations Cell (IARC) under the Dean of Resources and Alumni Affairs (DORA), IIT Roorkee. RAIONZO is a magnanimous platform which gives you an incredible opportunity to learn from the experiences of our alumni. This knowledge plays a very important role in guiding you to choose the right path in the future.
+                        </p></div>
+                     </div>
+                </div>
+        </div>
+
+      );
+    }
+  }
+
+
+  export default CONTAINER3;
