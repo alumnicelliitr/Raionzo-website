43cae2bcfa38ce6dbe737a419429655574928be9
abhayraghuwanshi <araghuwanshi@ee.iitr.ac.in>
2019-06-05 19:50:53 +0530

    css completed

--- website/src/components/container_3.js
+++ website/src/components/container_3.js
@@ -10,7 +10,7 @@ class CONTAINER3 extends React.Component {
             <div className="container-3-child-heading"> About Raionzo</div>
                 <div className="container-3-parent">
                     
-                    <div className="container-3-child-1" ><img className="container-3-child" src={group}></img></div> 
+                    <div className="container-3-child-1" ><img className="container-3-child" src={group} alt=""></img></div> 
                     
                     <div className="container-3-child-2">
                         <div className="container-3-child-body"> <p>RAIONZO, the student-alumni fest being organized for the first time this year by the Institute Alumni Relations Cell (IARC) under the Dean of Resources and Alumni Affairs (DORA), IIT Roorkee. RAIONZO is a magnanimous platform which gives you an incredible opportunity to learn from the experiences of our alumni. This knowledge plays a very important role in guiding you to choose the right path in the future.
--- website/src/components/container_4.js
+++ website/src/components/container_4.js
@@ -13,10 +13,8 @@ class CONTAINER4 extends React.Component {
                         <div className="container-4-child-body">The Institute Alumni Relations Cell is a student body working under the aegis of Dean of Resources and Alumni Affairs (DORA), IIT Roorkee. Team IARC aims to provide students various opportunities to interact with alumni. This gives the students a lot to learn, the alumni a chance to give back to the institute and help the students. Our team strives hard to achieve this and has taken up many initiatives to do so.
                         </div>
                     </div>
-                    <div className="container-4-child-2" ><img className="container-4-child" src={iarcblack}></img>
+                    <div className="container-4-child-2" ><img className="container-4-image" src={iarcblack} alt=""></img>
                     </div> 
-                    
-
             </div>
         </div>
 
