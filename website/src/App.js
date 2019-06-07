3340927807d29d61082409d441c1032a6be25d69
Aniket21mathur <amathur@ec.iitr.ac.in>
2019-06-07 15:05:30 +0530

    deleting and removing unused files and code

--- website/src/App.js
+++ website/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from './components/NavBar.js';
 import Footer from './components/Footer.js';
-import './App.css';
 import MainContainer from './components/maincontainer.js'
 
 function App() {
