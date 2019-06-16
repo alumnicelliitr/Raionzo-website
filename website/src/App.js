c8fd67b4517f51f79eaf3324457ca724b3d2f7aa
Kunal Mohan <kunalmohan99@gmail.com>
2019-06-16 16:51:20 +0530

    Home page heading corrected

---
+++ website/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainContainer from './Home/maincontainer.js';
+
+function App() {
+  return (
+  <div className="app">
+    <MainContainer/>
+    
+  
+ </div>
+  );
+}
+
+export default App;
