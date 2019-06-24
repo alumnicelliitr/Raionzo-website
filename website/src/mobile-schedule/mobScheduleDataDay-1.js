ba97118646e2752df657d61a290d6bd4efc1872f
abhayraghuwanshi <araghuwanshi@ee.iitr.ac.in>
2019-06-24 10:48:10 +0530

    mobile schedule added

---
+++ website/src/mobile-schedule/mobScheduleDataDay-1.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './mobileschedule.css';
+import ScheduleDATA from './mobScheduleData'
+
+
+
+
+class MobileScheduleDay1 extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state=
+            {
+                mobScheduleHeading1:"Leadership Talk",
+                mobScheduleHeading2:"Panel Discussion",
+                mobScheduleHeading3:"Workshop",
+            };
+      }
+
+    render(){return(
+        <div>
+
+            <div className="mob-Schedule-Box">
+            <div className="mobile_schedule_heading">{this.state.mobScheduleHeading1} </div>
+            <ScheduleDATA schedulecol1="Speaker" schedulecol2="LHC 101" schedulecol3="10:00 AM"/>
+            <ScheduleDATA schedulecol1="Speaker" schedulecol2="LHC 101" schedulecol3="10:00 AM"/>
+            <ScheduleDATA schedulecol1="Speaker" schedulecol2="LHC 101" schedulecol3="10:00 AM"/>
+        </div>
+        <div className="mob-Schedule-Box">
+            <div className="mobile_schedule_heading">{this.state.mobScheduleHeading2} </div>
+            <ScheduleDATA schedulecol1="International Affair of India" schedulecol2="LHC 103" schedulecol3="2:00 PM"/>
+            <ScheduleDATA schedulecol1="xyz" schedulecol2="LHC 101" schedulecol3="10:00 AM"/>
+            <ScheduleDATA schedulecol1="abc" schedulecol2="LHC 101" schedulecol3="10:00 AM"/>
+        </div>
+        <div className="mob-Schedule-Box">
+            <div className="mobile_schedule_heading">{this.state.mobScheduleHeading3} </div>
+            <ScheduleDATA schedulecol1="Finance" schedulecol2="LHC 103" schedulecol3="3:00 -7:00 PM"/>
+            <ScheduleDATA schedulecol1="design" schedulecol2="LHC 102" schedulecol3="3:00-7:00 PM"/>
+        </div>
+        </div>
+        );}
+    }
+
+    export default MobileScheduleDay1;
---
+++ website/src/mobile-schedule/mobScheduleDataDay2.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './mobileschedule.css';
+import ScheduleDATA from './mobScheduleData'
+
+
+    class MobileScheduleDay2 extends React.Component{
+        constructor(props) {
+            super(props);
+            this.state=
+                {
+                    mobScheduleHeadingDay1:"Panel Discussion",
+                    mobScheduleHeadingDay2:"Placement And Internship Talks",
+                    mobScheduleHeadingDay3:"Workshop",
+                    mobScheduleHeadingDay4:"Cult Night"
+                };
+          }
+    
+        render(){return(
+            <div>
+                <div className="mob-Schedule-Box">
+                <div className="mobile_schedule_heading">{this.state.mobScheduleHeadingDay1} </div>
+                    <ScheduleDATA schedulecol1="Speaker" schedulecol2="LHC 101" schedulecol3="10:00 AM"/>
+                </div>
+                <div className="mob-Schedule-Box">
+                <div className="mobile_schedule_heading">{this.state.mobScheduleHeadingDay2} </div>
+                    <ScheduleDATA schedulecol1="Speaker" schedulecol2="LHC 101" schedulecol3="10:00 AM"/>
+                </div>
+                <div className="mob-Schedule-Box">
+                <div className="mobile_schedule_heading">{this.state.mobScheduleHeadingDay3} </div>
+                    <ScheduleDATA schedulecol1="Web-D" schedulecol2="LHC 101" schedulecol3="10:00 AM"/>
+                    <ScheduleDATA schedulecol1="ML/AI" schedulecol2="LHC 101" schedulecol3="10:00 AM"/>
+                </div>
+                <div className="mob-Schedule-Box">
+                <div className="mobile_schedule_heading">{this.state.mobScheduleHeadingDay4 } </div>
+                    <ScheduleDATA schedulecol1="Cult Night" schedulecol2="Divine" schedulecol3="10:00 PM"/>
+                </div>
+
+           
+            </div>
+            );}
+        }
+export default MobileScheduleDay2 ;
