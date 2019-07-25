1462fdd1564a535c5c665450ff247fe6f57cb478
dinesh196 <chavandinesh196@gmail.com>
2019-07-25 16:05:20 +0530

    Updates

---
+++ precache-manifest.95ae8a467d55e5ccc9c91bb5cf0970a1.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1f413020c36ba8e58afa84b73563e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e699d46770b34450ebfe",
+    "url": "/static/css/main.ff0ab9f5.chunk.css"
+  },
+  {
+    "revision": "d4a9b03b6dc1f32a136f",
+    "url": "/static/js/2.119b30a4.chunk.js"
+  },
+  {
+    "revision": "e699d46770b34450ebfe",
+    "url": "/static/js/main.aeafa7ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a361f720abf18a1c8896dcef31979d5f",
+    "url": "/static/media/Design.a361f720.pdf"
+  },
+  {
+    "revision": "5fa06b75856d105804c62234c011b09e",
+    "url": "/static/media/Finance.5fa06b75.pdf"
+  },
+  {
+    "revision": "01ca965b69c09860123f73d31c04cf70",
+    "url": "/static/media/Group.01ca965b.png"
+  },
+  {
+    "revision": "4a5ff164da7eeb636a76d4b851d5545c",
+    "url": "/static/media/Idea-pitch.4a5ff164.pdf"
+  },
+  {
+    "revision": "48e2b778fecbb5ae0bfbb3ef9eb8c434",
+    "url": "/static/media/Int Affairs.48e2b778.pdf"
+  },
+  {
+    "revision": "ae2269b5ae67010cbad36fed49cc3303",
+    "url": "/static/media/ML.ae2269b5.pdf"
+  },
+  {
+    "revision": "eb47877614f063108be299fddbdfdbb7",
+    "url": "/static/media/Slide9.eb478776.png"
+  },
+  {
+    "revision": "851fe7014ecba0e82e5f5fc256350bb2",
+    "url": "/static/media/Women in Tech.851fe701.pdf"
+  },
+  {
+    "revision": "728b5f85ce5af32ffd3f7db382a76192",
+    "url": "/static/media/five.728b5f85.png"
+  },
+  {
+    "revision": "0818aea9a9a4965c5318203fc3c281c0",
+    "url": "/static/media/four.0818aea9.png"
+  },
+  {
+    "revision": "3c7ae23e6010a7daa4c06edc68ed5ea7",
+    "url": "/static/media/iarcblack.3c7ae23e.png"
+  },
+  {
+    "revision": "6adbb442a0acb6291b773f531a562fb3",
+    "url": "/static/media/iitrlogo.6adbb442.png"
+  },
+  {
+    "revision": "31f6b199c280f775ae562899191ea601",
+    "url": "/static/media/one.31f6b199.png"
+  },
+  {
+    "revision": "5b7ceed6da56a0a12b11dd2871ed4ce2",
+    "url": "/static/media/pankaj.5b7ceed6.jpeg"
+  },
+  {
+    "revision": "ba0b246b410cb8735bcde66206dea8a2",
+    "url": "/static/media/rajnish.ba0b246b.jpeg"
+  },
+  {
+    "revision": "6c0bd860344cde364cfa617b8c987bb4",
+    "url": "/static/media/six.6c0bd860.png"
+  },
+  {
+    "revision": "34d8d70014ef7697ca4242f5ffd3a058",
+    "url": "/static/media/three.34d8d700.png"
+  },
+  {
+    "revision": "a634f7719dab53dc3d3db21ddc0ab5e1",
+    "url": "/static/media/two.a634f771.png"
+  }
+]);
